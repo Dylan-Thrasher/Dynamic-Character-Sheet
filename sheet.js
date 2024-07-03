3d62731fdd9566464c981215045b57582c5dd062
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-07-02 23:03:10 -0400

    linking sheet to google sheets csv

---
+++ sheet.js
@@ -0,0 +1,5 @@
+let characterData = [];
+
+function LoadSheet() {
+    const file = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIvCXjJPwM1bnO-O9CmbB6VtbYLmVzBFpPohg3Bb2vfDxhoZwS-tJIZgyC2KagCiOGU3Owlky6jddx/pub?gid=0&single=true&output=csv"
+}
