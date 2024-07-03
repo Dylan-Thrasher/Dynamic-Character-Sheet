d89bb17279f0dafad8d4e7630f0261653559b207
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-07-02 23:06:19 -0400

    set papaparse to pull csv data from google sheet

--- sheet.js
+++ sheet.js
@@ -2,4 +2,13 @@ let characterData = [];
 
 function LoadSheet() {
     const file = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTIvCXjJPwM1bnO-O9CmbB6VtbYLmVzBFpPohg3Bb2vfDxhoZwS-tJIZgyC2KagCiOGU3Owlky6jddx/pub?gid=0&single=true&output=csv"
+
+    Papa.parse(file, {
+        download: true,
+        header: true,
+        complete: function (results) {
+            characterData = results.data;
+            PopulateData(characterData);
+        }
+    })
 }
