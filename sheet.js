6d813cfdc6abf2afea72eba36ca27a1d5d4e95a5
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-07-03 23:51:02 -0400

    added UpdateModifiers function

--- sheet.js
+++ sheet.js
@@ -16,7 +16,7 @@ function LoadSheet() {
 function PopulateData(data) {
     let playerList;
     let uniquePlayers = [];
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         // if player name does not exist, it will add the player to the list
         if (!uniquePlayers.includes(data[i].PlayerName)) {
             uniquePlayers.push(data[i].PlayerName);
@@ -81,3 +81,19 @@ function UpdateAbilityScores() {
     }
 }
 
+function UpdateModifiers() {
+    let strScore = document.getElementById("strScore").value;
+    document.getElementById("strMod").value = Math.floor((strScore - 10)/2);
+    let dexScore = document.getElementById("dexScore").value;
+    document.getElementById("dexMod").value = Math.floor((dexScore - 10)/2);
+    let conScore = document.getElementById("conScore").value;
+    document.getElementById("conMod").value = Math.floor((conScore - 10)/2);
+    let intScore = document.getElementById("intScore").value;
+    document.getElementById("intMod").value = Math.floor((intScore - 10)/2);
+    let wisScore = document.getElementById("wisScore").value;
+    document.getElementById("wisMod").value = Math.floor((wisScore - 10)/2);
+    let chaScore = document.getElementById("chaScore").value;
+    document.getElementById("chaMod").value = Math.floor((chaScore - 10)/2);
+    SetSkills();
+    UpdateAC();
+  }
