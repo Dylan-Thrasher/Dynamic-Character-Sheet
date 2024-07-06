500edc0e33cb6916d2e6c4a17d698ec51858c3e8
Dylan Thrasher <thrasher_dylan@yahoo.com>
2024-07-05 23:03:14 -0400

    added SetSkills function

--- sheet.js
+++ sheet.js
@@ -160,60 +160,198 @@ function LoadGear() {
 function UpdateAC() {
     let armor = document.getElementById("equippedArmor").value;
     if (armor == 'padded') {
-      document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 11;
+        document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 11;
     } else if (armor == 'leather') {
-      document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 11;
+        document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 11;
     } else if (armor == 'studded') {
-      document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 12;
+        document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 12;
     } else if (armor == 'hide') {
-      let tempArmor = parseInt(document.getElementById("dexMod").value) + 12;
-      if (tempArmor > 14) {
-         document.getElementById("armorClass").value = 14;
-      } else {
-       document.getElementById("armorClass").value = tempArmor; 
-      }
+        let tempArmor = parseInt(document.getElementById("dexMod").value) + 12;
+        if (tempArmor > 14) {
+            document.getElementById("armorClass").value = 14;
+        } else {
+            document.getElementById("armorClass").value = tempArmor;
+        }
     } else if (armor == 'chain') {
-      let tempArmor = parseInt(document.getElementById("dexMod").value) + 13;
-      if (tempArmor > 15) {
-         document.getElementById("armorClass").value = 15;
-      } else {
-       document.getElementById("armorClass").value = tempArmor; 
-      }
+        let tempArmor = parseInt(document.getElementById("dexMod").value) + 13;
+        if (tempArmor > 15) {
+            document.getElementById("armorClass").value = 15;
+        } else {
+            document.getElementById("armorClass").value = tempArmor;
+        }
     } else if (armor == 'scale') {
-      let tempArmor = parseInt(document.getElementById("dexMod").value) + 14;
-      if (tempArmor > 16) {
-         document.getElementById("armorClass").value = 16;
-      } else {
-       document.getElementById("armorClass").value = tempArmor; 
-      }
+        let tempArmor = parseInt(document.getElementById("dexMod").value) + 14;
+        if (tempArmor > 16) {
+            document.getElementById("armorClass").value = 16;
+        } else {
+            document.getElementById("armorClass").value = tempArmor;
+        }
     } else if (armor == 'breastplate') {
-      let tempArmor = parseInt(document.getElementById("dexMod").value) + 14;
-      if (tempArmor > 16) {
-         document.getElementById("armorClass").value = 16;
-      } else {
-       document.getElementById("armorClass").value = tempArmor; 
-      }
+        let tempArmor = parseInt(document.getElementById("dexMod").value) + 14;
+        if (tempArmor > 16) {
+            document.getElementById("armorClass").value = 16;
+        } else {
+            document.getElementById("armorClass").value = tempArmor;
+        }
     } else if (armor == 'halfplate') {
-      let tempArmor = parseInt(document.getElementById("dexMod").value) + 15;
-      if (tempArmor > 17) {
-         document.getElementById("armorClass").value = 17;
-      } else {
-       document.getElementById("armorClass").value = tempArmor; 
-      }
+        let tempArmor = parseInt(document.getElementById("dexMod").value) + 15;
+        if (tempArmor > 17) {
+            document.getElementById("armorClass").value = 17;
+        } else {
+            document.getElementById("armorClass").value = tempArmor;
+        }
     } else if (armor == 'ringmail') {
-      document.getElementById("armorClass").value = 14;
+        document.getElementById("armorClass").value = 14;
     } else if (armor == 'chainmail') {
-      document.getElementById("armorClass").value = 16;
+        document.getElementById("armorClass").value = 16;
     } else if (armor == 'splint') {
-      document.getElementById("armorClass").value = 17;
+        document.getElementById("armorClass").value = 17;
     } else if (armor == 'plate') {
-      document.getElementById("armorClass").value = 18;
+        document.getElementById("armorClass").value = 18;
     } else {
-      document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 10;
+        document.getElementById("armorClass").value = parseInt(document.getElementById("dexMod").value) + 10;
     }
     if (document.getElementById("equippedShield").value == 'shield') {
-      document.getElementById("armorClass").value = parseInt(document.getElementById("armorClass").value) + 2;
+        document.getElementById("armorClass").value = parseInt(document.getElementById("armorClass").value) + 2;
     } else {
-      document.getElementById("armorClass").value = parseInt(document.getElementById("armorClass").value);
+        document.getElementById("armorClass").value = parseInt(document.getElementById("armorClass").value);
     }
-  }
+}
+
+function SetSkills() {
+    let profBonus = parseInt(document.getElementById("profBonus").value);
+    let strMod = parseInt(document.getElementById("strMod").value);
+    let dexMod = parseInt(document.getElementById("dexMod").value);
+    let conMod = parseInt(document.getElementById("conMod").value);
+    let intMod = parseInt(document.getElementById("intMod").value);
+    let wisMod = parseInt(document.getElementById("wisMod").value);
+    let chaMod = parseInt(document.getElementById("chaMod").value);
+
+    if (document.getElementById("acroProf").checked == true) {
+        document.getElementById("acroProf").value = true;
+        document.getElementById("acroScore").value = dexMod + profBonus;
+    } else {
+        document.getElementById("acroProf").value = false;
+        document.getElementById("acroScore").value = dexMod;
+    }
+    if (document.getElementById("animProf").checked == true) {
+        document.getElementById("animProf").value = true;
+        document.getElementById("animScore").value = wisMod + profBonus;
+    } else {
+        document.getElementById("animProf").value = false;
+        document.getElementById("animScore").value = wisMod;
+    }
+    if (document.getElementById("arcaProf").checked == true) {
+        document.getElementById("arcaProf").value = true;
+        document.getElementById("arcaScore").value = intMod + profBonus;
+    } else {
+        document.getElementById("arcaProf").value = false;
+        document.getElementById("arcaScore").value = intMod;
+    }
+    if (document.getElementById("athlProf").checked == true) {
+        document.getElementById("athlProf").value = true;
+        document.getElementById("athlScore").value = strMod + profBonus;
+    } else {
+        document.getElementById("athlProf").value = false;
+        document.getElementById("athlScore").value = strMod;
+    }
+    if (document.getElementById("decProf").checked == true) {
+        document.getElementById("decProf").value = true;
+        document.getElementById("decScore").value = chaMod + profBonus;
+    } else {
+        document.getElementById("decProf").value = false;
+        document.getElementById("decScore").value = chaMod;
+    }
+    if (document.getElementById("hisProf").checked == true) {
+        document.getElementById("hisProf").value = true;
+        document.getElementById("hisScore").value = intMod + profBonus;
+    } else {
+        document.getElementById("hisProf").value = false;
+        document.getElementById("hisScore").value = intMod;
+    }
+    if (document.getElementById("insProf").checked == true) {
+        document.getElementById("insProf").value = true;
+        document.getElementById("insScore").value = wisMod + profBonus;
+    } else {
+        document.getElementById("insProf").value = false;
+        document.getElementById("insScore").value = wisMod;
+    }
+    if (document.getElementById("intiProf").checked == true) {
+        document.getElementById("intiProf").value = true;
+        document.getElementById("intiScore").value = chaMod + profBonus;
+    } else {
+        document.getElementById("intiProf").value = false;
+        document.getElementById("intiScore").value = chaMod;
+    }
+    if (document.getElementById("invProf").checked == true) {
+        document.getElementById("invProf").value = true;
+        document.getElementById("invScore").value = intMod + profBonus;
+    } else {
+        document.getElementById("invProf").value = false;
+        document.getElementById("invScore").value = intMod;
+    }
+    if (document.getElementById("medProf").checked == true) {
+        document.getElementById("medProf").value = true;
+        document.getElementById("medScore").value = wisMod + profBonus;
+    } else {
+        document.getElementById("medProf").value = false;
+        document.getElementById("medScore").value = wisMod;
+    }
+    if (document.getElementById("natProf").checked == true) {
+        document.getElementById("natProf").value = true;
+        document.getElementById("natScore").value = intMod + profBonus;
+    } else {
+        document.getElementById("natProf").value = false;
+        document.getElementById("natScore").value = intMod;
+    }
+    if (document.getElementById("percProf").checked == true) {
+        document.getElementById("percProf").value = true;
+        document.getElementById("percScore").value = wisMod + profBonus;
+    } else {
+        document.getElementById("percProf").value = false;
+        document.getElementById("percScore").value = wisMod;
+    }
+    if (document.getElementById("perfProf").checked == true) {
+        document.getElementById("perfProf").value = true;
+        document.getElementById("perfScore").value = chaMod + profBonus;
+    } else {
+        document.getElementById("perfProf").value = false;
+        document.getElementById("perfScore").value = chaMod;
+    }
+    if (document.getElementById("persProf").checked == true) {
+        document.getElementById("persProf").value = true;
+        document.getElementById("persScore").value = chaMod + profBonus;
+    } else {
+        document.getElementById("persProf").value = false;
+        document.getElementById("persScore").value = chaMod;
+    }
+    if (document.getElementById("relProf").checked == true) {
+        document.getElementById("relProf").value = true;
+        document.getElementById("relScore").value = intMod + profBonus;
+    } else {
+        document.getElementById("relProf").value = false;
+        document.getElementById("relScore").value = intMod;
+    }
+    if (document.getElementById("sleiProf").checked == true) {
+        document.getElementById("sleiProf").value = true;
+        document.getElementById("sleiScore").value = dexMod + profBonus;
+    } else {
+        document.getElementById("sleiProf").value = false;
+        document.getElementById("sleiScore").value = dexMod;
+    }
+    if (document.getElementById("steProf").checked == true) {
+        document.getElementById("steProf").value = true;
+        document.getElementById("steScore").value = dexMod + profBonus;
+    } else {
+        document.getElementById("steProf").value = false;
+        document.getElementById("steScore").value = dexMod;
+    }
+    if (document.getElementById("survProf").checked == true) {
+        document.getElementById("survProf").value = true;
+        document.getElementById("survScore").value = wisMod + profBonus;
+    } else {
+        document.getElementById("survProf").value = false;
+        document.getElementById("survScore").value = wisMod;
+    }
+
+}
